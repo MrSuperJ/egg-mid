b85b027096e6f01f9f9e645d27d745eddd435a1c
superj <jingjing994@qq.com>
2018-05-01 14:12:23 +0800

    获取当前IP

--- app/controller/get_img.js
+++ app/controller/get_img.js
@@ -5,7 +5,7 @@ module.exports = app => {
     return class getImgController extends app.ApiController {
         index() {
             var data = [];
-            var main = 'http://192.168.1.101:7001/public/imgs/0';
+            var main = getIPAdress() + '/public/imgs/0';
             for (var i = 1; i < 5; i++) {
                 var arr = [];
                 for (var j = 1; j < 50; j++) {
@@ -18,7 +18,18 @@ module.exports = app => {
             }
             this.success(data)
         }
+    }
 
-
+    function getIPAdress() {
+        var interfaces = require('os').networkInterfaces();
+        for (var devName in interfaces) {
+            var iface = interfaces[devName];
+            for (var i = 0; i < iface.length; i++) {
+                var alias = iface[i];
+                if (alias.family === 'IPv4' && alias.address !== '127.0.0.1' && !alias.internal) {
+                    return alias.address;
+                }
+            }
+        }
     }
 }
